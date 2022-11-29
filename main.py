db21d5d6c1d0ebc71a31c09570022d9a0f626486
klemmm922 <clement.letizia@epfedu.fr>
2022-11-29 15:13:38 +0100

    changing main for ga

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET"])
 def hello_world():
-    return "Hello, world!"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async 
+src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'G-XLPQT1T2SC');
+</script>
+ """
+ return prefix_google + "Hello World"
