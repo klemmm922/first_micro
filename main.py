64106024cd52f30deddabb3f3db8173c147ee7a6
klemmm922 <clement.letizia@epfedu.fr>
2023-01-07 17:19:46 +0100

    log time added

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import requests as requests
 from flask import Flask,render_template
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd   
+from pytrends.request import TrendReq
+import datetime
+import time
+from functools import wraps
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -19,7 +25,6 @@ def home():
     </script>
     """
     return render_template("home.html") + prefix_google
-
 
 @app.route('/logger', methods=["GET","POST"])
 def logger(): 
@@ -84,3 +89,129 @@ def oauth():
     #visitors = get_visitors(response)
     #return render_template("oauth.html", visitors=str(visitors))
     return render_template("oauth.html")
+
+@app.route('/pytrends',methods=["GET"])
+def pytrends():
+  pytrend = TrendReq()
+  kw_list = ["Facebook","Apple","Google","Microsoft"]
+  pytrend.build_payload(kw_list, timeframe='today 3-m')
+
+  regiondf = pytrend.interest_over_time()
+  regiondf = regiondf.sort_values(by='Facebook', ascending=False)
+
+  df_facebook = regiondf[['Facebook']]
+  df_apple = regiondf[['Apple']]
+  df_google = regiondf[['Google']]
+  df_microsoft = regiondf[['Microsoft']]
+
+  df_date = regiondf.index.values.tolist()
+
+  ts = [element/1e9 for element in df_date]
+  date = [datetime.datetime.fromtimestamp(element) for element in ts]
+  days = [element.date() for element in date]
+  months = [element.month for element in days]
+
+  params = {
+        "type": 'line',
+        "data": {
+            "labels": months,
+            "datasets": [{
+                "label": "Facebook",
+                "data": df_facebook,
+                "borderColor": '#3e95cd',
+                "fill": 'false',
+            },
+                {
+                "label": "Apple",
+                "data": df_apple,
+                "borderColor": '#ffce56',
+                    "fill": 'false',
+            },
+              {
+              "label": "Google",
+              "data": df_google,
+              "borderColor": '#8e5ea2',
+                  "fill": 'false',
+            },
+              {
+              "label": "Microsoft",
+              "data": df_microsoft,
+              "borderColor": '#e8c3b9',
+                  "fill": 'false',
+            }
+            ]
+        },
+        "options": {
+            "title": {
+                "display": 'true',
+                "text": 'Trend comparison'
+            },
+            "scales": {
+                "yAxes": [{
+                    "ticks": {
+                          "beginAtZero": 'true'
+                          }
+                }]
+            }
+        }
+    }
+
+
+  prefix_chartjs = """
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+  <canvas id="myChart" width="1200px" height="700px"></canvas>""" + f"""
+  <script>
+  var ctx = document.getElementById('myChart');
+  var myChart = new Chart(ctx, {params});
+  </script>
+  """
+
+  #return prefix_chartjs
+  
+  return regiondf.to_html(classes='table table-striped table-hover table-condensed table-responsive') + prefix_chartjs
+
+  # return regiondf.to_html()
+  #return regiondf.to_html(classes='table table-striped table-hover table-condensed table-responsive')
+
+
+def log_execution_time(func):
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+      start_time = time.time()
+      result = func(*args, **kwargs)
+      end_time = time.time()
+      print(f"Execution time: {end_time - start_time} seconds")
+      return result
+  return wrapper
+
+
+@app.route('/shakespear',methods=["GET"])
+@log_execution_time
+def shakespear():
+  with open('shakespeare.txt', 'r') as f:
+        text = f.read()
+
+  word_counts = {}
+  for word in text.split():
+      if word in word_counts:
+          word_counts[word] += 1
+      else:
+          word_counts[word] = 1
+  
+  return render_template('shakespear.html', word_counts=word_counts)
+
+
+@app.route('/countShakespear',methods=["GET"])
+@log_execution_time
+def countShakespear():
+  
+  with open('shakespeare.txt', 'r') as f:
+      text = f.read()
+
+  # Split the text into words
+  words = text.split()
+
+  # Use the Counter function to count the number of occurrences of each word
+  word_counts = Counter(words)
+
+  return render_template('shakespear.html', word_counts=word_counts)
